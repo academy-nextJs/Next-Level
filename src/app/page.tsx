f112d5f6eba0a73808a9f45848291d865b51be02
AbbasRostami <abbas.cracker@gmail.com>
2025-04-28 00:31:34 +0330

    fix: add dark background to main container in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "@/components/Landing/HeroSection";
 export default function Home() {
   
   return (
-    <div className="px-2 md:px-8 py-4">
+    <div className="px-2 md:px-8 py-4 dark:bg-[#0a192f]">
       <Header />
       <HeroSection />
       <CustomSwiper />
