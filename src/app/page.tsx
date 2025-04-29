e1c72265df7d25e2f9880e9351193b250a4526d1
AbbasRostami <abbas.cracker@gmail.com>
2025-04-30 03:23:47 +0330

    add componets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import { LandinCenter } from "@/components/landinCenter";
 import { Landingfooter } from "@/components/Landing-footer";
 import Image from "next/image";
 import BayourtBiggest from "@/components/Landing/BayourtBiggest";
+import Customers from "@/components/Landing/Customers";
 
 export default function Home() {
-  
   return (
     <div className="px-2 md:px-8 py-4 dark:bg-[#0a192f]">
       <Header />
       <HeroSection />
       <CustomSwiper />
       {/* <LandinCenter />  */}
-     <BayourtBiggest/>
+      <BayourtBiggest />
+      <Customers />
     </div>
-
   );
 }
