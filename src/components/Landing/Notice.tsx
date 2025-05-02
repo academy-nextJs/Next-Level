b092187e358beffdf4100efaa0b0abdc9ca8e349
AbbasRostami <abbas.cracker@gmail.com>
2025-05-02 18:38:24 +0330

    feat: add Notice component with promotional message and button

---
+++ src/components/Landing/Notice.tsx
@@ -0,0 +1,29 @@
+export default function Notice() {
+  return (
+    <div className="flex justify-center items-center px-4 my-20">
+      <div className="relative z-20 w-full max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row items-center justify-evenly gap-3 md:gap-10">
+          <button className="bg-gradient-to-r from-orange-500 to-orange-600 cursor-pointer rounded-2xl text-lg lg:text-xl px-4 lg:px-6 py-5 text-white font-semibold hover:scale-105 hover:shadow-lg transition-all duration-300 shadow-md transform-glow">
+            🏡 میخوام آگهی بذارم!
+          </button>
+
+          <div className="text-right text-medium md:text-lg lg:text-xl xl:text-2xl dark:bg-gray-800 bg-[#FFEFD9] translate-x-7 drop-shadow-2xl rounded-r-[10px] rounded-l-[110px] w-fit z-10 px-8 xl:px-18 py-12 font-medium leading-relaxed backdrop-blur-sm relative overflow-hidden">
+            <div className="absolute inset-0 bg-gradient-to-r from-transparent to-white/10 w-full h-full pointer-events-none" />
+            <span className="relative z-10">
+              اگه تو هم خونه یا ملکی داری که میخوای بفروشی یا اجاره بدی
+              <span className="text-orange-600 whitespace-nowrap">
+                {" "}
+                الان وقتشه ! ✨
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <div
+          className="absolute border-4 border-amber-700/80 rounded-l-xl rounded-r-full w-10/12 lg:w-4/5 left-1/2 top-1/2 -translate-x-[40%] -translate-y-1/2 pointer-events-none shadow-xl"
+          style={{ height: "calc(100% + 3rem)" }}
+        />
+      </div>
+    </div>
+  );
+}
