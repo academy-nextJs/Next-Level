096ad50ba2ba9d9019154ab5813baa572d4bb7e2
AbbasRostami <abbas.cracker@gmail.com>
2025-05-05 01:36:34 +0330

    refactor: update useServerData to use endpoint parameter and improve error logging

--- src/utils/hooks/useServerData.ts
+++ src/utils/hooks/useServerData.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { unstable_cache } from "next/cache";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
 export const useServerData = (
-  url: string,
+  endpoint: string,
   cacheKey: string,
   revalidateTime: number = 60
 ) => {
   const fetchData = unstable_cache(
     async () => {
       try {
-        const { data } = await axios.get(url);
+        const { data } = await axios.get(`${API_URL}${endpoint}`);
         return data;
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      } catch (error: any) {
+        console.error("Error fetching data:", error?.message);
         throw new Error("Data fetch failed");
       }
     },
