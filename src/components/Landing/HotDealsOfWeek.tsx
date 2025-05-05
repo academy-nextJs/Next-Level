ea12d3e150e7b1a4849754628cd429c4e8abbe9c
AbbasRostami <abbas.cracker@gmail.com>
2025-05-05 18:41:00 +0330

    feat: refactor HotDealsOfWeek component and add HotDealsOfWeekSwiper for improved UI and functionality

--- src/components/Landing/HotDealsOfWeek.tsx
+++ src/components/Landing/HotDealsOfWeek.tsx
@@ -1,64 +1,25 @@
-"use client";
-import {
-  IoIosArrowBack,
-  IoIosArrowRoundBack,
-  IoIosArrowRoundForward,
-  IoMdBed,
-} from "react-icons/io";
-import { FaCircle, FaShower } from "react-icons/fa6";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Button } from "@heroui/react";
-import { IoLocationOutline } from "react-icons/io5";
-import { MdFamilyRestroom } from "react-icons/md";
-const cards = [
-  {
-    title: " ویلای دوبلکس ساحلی  ",
-    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
-    locate: "مازندران ، بابلسر",
-    cost: "۱,۷۵۰,۰۰۰",
-    discount: "۲.۲۵۰.۰۰۰",
-  },
-  {
-    title: " ویلای دوبلکس ساحلی  ",
-    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
-    locate: "مازندران ، بابلسر",
-    cost: "۱,۷۵۰,۰۰۰",
-    discount: "۲.۲۵۰.۰۰۰",
-  },
-  {
-    title: " ویلای دوبلکس ساحلی  ",
-    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
-    locate: "مازندران ، بابلسر",
-    cost: "۱,۷۵۰,۰۰۰",
-    discount: "۲.۲۵۰.۰۰۰",
-  },
-  {
-    title: " ویلای دوبلکس ساحلی  ",
-    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
-    locate: "مازندران ، بابلسر",
-    cost: "۱,۷۵۰,۰۰۰",
-    discount: "۲.۲۵۰.۰۰۰",
-  },
-  {
-    title: " ویلای دوبلکس ساحلی  ",
-    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
-    locate: "مازندران ، بابلسر",
-    cost: "۱,۷۵۰,۰۰۰",
-    discount: "۲.۲۵۰.۰۰۰",
-  },
-];
+import { IoIosArrowBack } from "react-icons/io";
+import { FaCircle } from "react-icons/fa6";
+import HotDealsOfWeekSwiper from "./Swiper/HotDealsOfWeekSwiper";
+import { useServerData } from "@/utils/hooks/useServerData";
 
-export default function HotDealsOfWeek() {
+export default async function HotDealsOfWeek() {
+  const houses = await useServerData(
+    "/houses?limit=4&sort=last_updated&order=DESC&transactionType=rental",
+    "top-last_updated-houses",
+    60
+  );
+
+  console.log("houses:", houses);
   return (
     <div className="relative w-full mt-32">
       <div className="flex items-center justify-between rounded-xl px-4">
         <div className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-6 p-6">
           <div className="flex items-center gap-3">
-            <FaCircle className="text-orange-500 text-xs" />
+            <FaCircle
+              size={17}
+              className="text-orange-500 text-xs  animate-pulse-color"
+            />
             <h2 className="text-orange-700 text-xl md:text-2xl font-bold dark:text-amber-400">
               داغ‌ترین معاملات هفته
             </h2>
@@ -68,98 +29,18 @@ export default function HotDealsOfWeek() {
           </p>
         </div>
 
-        <Button
-          variant="light"
-          className="flex items-center shadow justify-center px-4 py-2 text-lg font-semibold text-gray-400 "
+        <button
+          className="flex items-center gap-2 cursor-pointer justify-center px-5 py-2 rounded-xl
+             text-lg font-semibold shadow-md transition-all duration-300
+             text-gray-600 bg-white hover:bg-amber-100
+             dark:text-gray-200 dark:bg-slate-800 dark:hover:bg-slate-700"
         >
           مشاهده همه
-          <IoIosArrowBack className="text-lg mr-1" />
-        </Button>
-      </div>
-
-      <div className="flex items-center justify-between gap-4">
-        <button className="custom-prev-Hot  cursor-pointer  flex items-center justify-center rounded-full border border-gray-100 shadow-md  hover:text-white transition">
-          <IoIosArrowRoundForward className="dark:text-amber-100" size={30} />
-        </button>
-
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          navigation={{
-            nextEl: ".custom-next-Hot",
-            prevEl: ".custom-prev-Hot",
-          }}
-          pagination={{
-            el: ".HotDeals",
-            clickable: true,
-          }}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          spaceBetween={20}
-          breakpoints={{
-            0: { slidesPerView: 1 },
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
-            1280: { slidesPerView: 4 },
-          }}
-        >
-          {cards.map((items, index) => (
-            <SwiperSlide key={index} className="flex justify-center p-4">
-              <div className="bg-[#FFFAF3] dark:bg-slate-900 rounded-xl shadow-xl py-4 mx-auto cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-amber-500/50">
-                <div className="relative perspective p-2">
-                  <img
-                    src={items.img}
-                    alt={items.title}
-                    className="w-full h-44 object-cover rounded-xl scale-x-115 bg-white shadow-xl p-1 border-2 border-gray-100 transition-all duration-300 ease-in-out hover:rotate-2 hover:scale-105 transform"
-                    style={{ transform: "perspective(800px) rotateY(24deg)" }}
-                  />
-                </div>
-                <div className="px-6 py-4 text-right space-y-5">
-                  <p className="text-lg text-gray-700 font-semibold dark:text-amber-200 transition-all duration-300 ease-in-out hover:text-orange-600">
-                    {items.title}
-                  </p>
-                  <p className="flex items-center text-gray-600 text-sm dark:text-amber-100">
-                    <IoLocationOutline
-                      size={26}
-                      className="  dark:text-white"
-                    />
-                    {items.locate}
-                  </p>
-                  <div className="flex justify-start gap-6 text-sm text-gray-700 mt-3">
-                    <div className="flex items-center gap-2">
-                      <IoMdBed size={30} className="dark:text-amber-50" />
-                      <span className="text-base dark:text-amber-100">۳</span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <FaShower size={25} className="dark:text-amber-50" />
-                      <span className="text-base dark:text-amber-100">۲</span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <MdFamilyRestroom
-                        size={25}
-                        className="dark:text-amber-50"
-                      />
-                      <span className="text-base dark:text-amber-100">۶</span>
-                    </div>
-                  </div>
-                  <div className="border border-gray-200 my-2"></div>
-                  <div className="flex items-center justify-between text-lg text-orange-600 font-semibold mt-4">
-                    <span>{items.cost} تومان</span>
-                    <p className="line-through decoration-orange-600 dark:text-gray-100 text-gray-400 text-base">
-                      {items.discount} تومان
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <button className="custom-next-Hot z-10  cursor-pointer    flex items-center justify-center rounded-full border border-gray-100 shadow-md  hover:text-white transition">
-          <IoIosArrowRoundBack className="dark:text-amber-100" size={30} />
+          <IoIosArrowBack className="text-xl transition-transform duration-300 group-hover:-translate-x-1" />
         </button>
       </div>
+
+      <HotDealsOfWeekSwiper houses={houses} />
       <div className="HotDeals flex justify-center mt-7  p-1 " />
     </div>
   );
---
+++ src/components/Landing/Swiper/HotDealsOfWeekSwiper.tsx
@@ -0,0 +1,161 @@
+"use client";
+import React from "react";
+import { FaShower } from "react-icons/fa";
+import {
+  IoIosArrowRoundForward,
+  IoMdBed,
+  IoIosArrowRoundBack,
+} from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
+import { MdFamilyRestroom } from "react-icons/md";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { HouseTypeProps } from "@/types/LandingType";
+const cards = [
+  {
+    title: " ویلای دوبلکس ساحلی  ",
+    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
+    locate: "مازندران ، بابلسر",
+    cost: "۱,۷۵۰,۰۰۰",
+    discount: "۲.۲۵۰.۰۰۰",
+  },
+  {
+    title: " ویلای دوبلکس ساحلی  ",
+    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
+    locate: "مازندران ، بابلسر",
+    cost: "۱,۷۵۰,۰۰۰",
+    discount: "۲.۲۵۰.۰۰۰",
+  },
+  {
+    title: " ویلای دوبلکس ساحلی  ",
+    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
+    locate: "مازندران ، بابلسر",
+    cost: "۱,۷۵۰,۰۰۰",
+    discount: "۲.۲۵۰.۰۰۰",
+  },
+  {
+    title: " ویلای دوبلکس ساحلی  ",
+    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
+    locate: "مازندران ، بابلسر",
+    cost: "۱,۷۵۰,۰۰۰",
+    discount: "۲.۲۵۰.۰۰۰",
+  },
+  {
+    title: " ویلای دوبلکس ساحلی  ",
+    img: "https://4kwallpapers.com/images/walls/thumbs_3t/22104.jpg",
+    locate: "مازندران ، بابلسر",
+    cost: "۱,۷۵۰,۰۰۰",
+    discount: "۲.۲۵۰.۰۰۰",
+  },
+];
+
+const HotDealsOfWeekSwiper = ({ houses }: HouseTypeProps) => {
+  const extendHouses = [
+    ...houses,
+    ...houses,
+    ...houses,
+    ...houses,
+    ...houses,
+    ...houses,
+  ];
+  return (
+    <div className=" relative gap-4 px-7">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation={{
+          nextEl: ".custom-next-off",
+          prevEl: ".custom-next-Hot",
+        }}
+        pagination={{
+          el: ".HotDeals",
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        spaceBetween={20}
+        breakpoints={{
+          0: { slidesPerView: 1 },
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1280: { slidesPerView: 4 },
+        }}
+      >
+        {extendHouses?.map((items, index) => (
+          <SwiperSlide
+            key={index}
+            className="flex justify-center p-4 rounded-xl "
+          >
+            <div className="bg-[#FFFAF3] dark:bg-neutral-900 rounded-xl shadow-xl py-4 px-2 mx-auto cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-amber-500/50">
+              <Swiper
+                modules={[Autoplay, Pagination]}
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }}
+                pagination={{ clickable: true }}
+                loop
+              >
+                {items.photos.map((photo: string, idx: number) => (
+                  <SwiperSlide key={idx}>
+                    <img
+                      src={photo}
+                      alt={`${items.title}-photo-${idx}`}
+                      className="w-full h-44 object-cover rounded-xl bg-white shadow-xl p-1 border-2 border-gray-100 transition-all duration-300 ease-in-out transform"
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+              <div className="px-6 py-4 text-right space-y-5">
+                <p className="text-lg text-gray-700 font-semibold dark:text-amber-200 transition-all duration-300 ease-in-out hover:text-orange-600">
+                  {items.title}
+                </p>
+                <p className="flex items-center truncate text-gray-600 text-sm dark:text-amber-100">
+                  <IoLocationOutline size={26} className="dark:text-white" />
+                  {items.address}
+                </p>
+                <div className="flex justify-start gap-6 text-sm text-gray-700 mt-3">
+                  <div className="flex items-center gap-2">
+                    <IoMdBed size={25} className="dark:text-amber-50" />
+                    <span className="text-base dark:text-amber-100">
+                      {items.rooms}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <FaShower size={25} className="dark:text-amber-50" />
+                    <span className="text-base dark:text-amber-100">
+                      {items.bathrooms}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MdFamilyRestroom
+                      size={25}
+                      className="dark:text-amber-50"
+                    />
+                    <span className="text-base dark:text-amber-100">
+                      {items.capacity}
+                    </span>
+                  </div>
+                </div>
+                <div className="border border-gray-200 my-2"></div>
+                <div className="flex items-center justify-between text-lg text-orange-600 font-semibold mt-4">
+                  <span>{items.price} تومان</span>
+                  <p className="line-through decoration-orange-600 dark:text-gray-100 text-gray-400 text-base">
+                    300000 تومان
+                  </p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default HotDealsOfWeekSwiper;
