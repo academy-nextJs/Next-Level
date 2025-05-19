fd25f531bbc71cca57519a893d0561b18ff68193
AbbasRostami <abbas.cracker@gmail.com>
2025-05-19 22:13:48 +0330

    Refactor Categories component to use CategoryProps for type safety in data fetching

--- src/components/Landing/Categories/Categories.tsx
+++ src/components/Landing/Categories/Categories.tsx
@@ -1,10 +1,12 @@
 import { useServerData } from "@/utils/hooks/useServerData";
 import CategorySwiper from "./CategorySwiper";
+import { CategoryProps } from "@/types/LandingType";
 
 export default async function Categories() {
-  const categories = await useServerData("/categories", "categories");
-
-  console.log("categories: ", categories);
+  const categories = await useServerData<CategoryProps[]>(
+    "/categories",
+    "categories"
+  );
 
   return <CategorySwiper categories={categories} />;
 }
