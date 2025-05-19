dc7af3ad043afd34daa48609a0c1eeb59d8823cf
AbbasRostami <abbas.cracker@gmail.com>
2025-05-19 22:25:57 +0330

    Refactor ClientWrapper component to enforce type safety with HouseReserveProps and improve data handling

--- src/app/(main)/houses-reserve/ClientWrapper.tsx
+++ src/app/(main)/houses-reserve/ClientWrapper.tsx
@@ -3,16 +3,21 @@ import { useSearchParams } from "next/navigation";
 import dynamic from "next/dynamic";
 import { useGet } from "@/utils/hooks/useReactQueryHooks";
 import SkeletonCards from "@/components/skeleton/SkeletonHouses";
+import { HouseReserveProps } from "@/types/HousesReserve";
 
 const FilterData = dynamic(() => import("./FilterData"));
 const ItemsList = dynamic(() => import("./ItemsList"));
 const MapHousesReserve = dynamic(() => import("./MapHousesReserve"));
 
-export default function ClientWrapper({ initialData }: { initialData: any }) {
+export default function ClientWrapper({
+  initialData,
+}: {
+  initialData: HouseReserveProps[];
+}) {
   const searchParams = useSearchParams();
   const params = Object.fromEntries(searchParams.entries());
 
-  const { data, isFetching } = useGet("/houses", params, {
+  const { data, isFetching } = useGet<HouseReserveProps[]>("/houses", params, {
     initialData,
     queryKey: ["houses", params],
     staleTime: 60000,
@@ -26,11 +31,11 @@ export default function ClientWrapper({ initialData }: { initialData: any }) {
       <div className="bg-white p-6 flex flex-col overflow-hidden dark:bg-[#0a192f]">
         <FilterData />
 
-        {isFetching ? <SkeletonCards /> : <ItemsList data={data} />}
+        {isFetching ? <SkeletonCards /> : <ItemsList data={data ?? []} />}
       </div>
 
       <div className="z-0 dark:bg-[#0a192f] p-4 lg:p-0 md:h-[calc(100vh-5rem)] min-h-[400px]">
-        <MapHousesReserve data={data} />
+        <MapHousesReserve data={data ?? []} />
       </div>
     </div>
   );
