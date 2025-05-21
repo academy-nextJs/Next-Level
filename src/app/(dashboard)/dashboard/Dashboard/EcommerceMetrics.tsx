3d25e6f1e45575f4472ade227ff3b882e7f3fe14
AbbasRostami <abbas.cracker@gmail.com>
2025-05-22 02:49:00 +0330

    Refactor EcommerceMetrics component to use Next.js Image for optimized loading and replace intro image with SVG asset, enhancing visual quality and performance.

--- src/app/(dashboard)/dashboard/Dashboard/EcommerceMetrics.tsx
+++ src/app/(dashboard)/dashboard/Dashboard/EcommerceMetrics.tsx
@@ -1,21 +1,21 @@
-import introImg from "./../../../../assets/Avatar1.png";
+import Image from "next/image";
+import introImg from "./../../../../assets/introImg.svg";
 
 export default function EcommerceMetrics() {
-
   return (
     <div className="flex flex-col rtl h-full w-full items-center shadow-xl bg-white border  border-gray-200 dark:hover:bg-gray-700/80 transition-all duration-300  rounded-2xl md:flex-col  hover:bg-gray-100  dark:border-gray-800 dark:bg-gray-900 ">
-      <img
+      <Image
         className="object-cover w-full rounded-t-lg h-65 md:h-auto md:w-50 md:rounded-none md:rounded-s-lg"
         src={introImg}
         alt="خوش آمدید"
       />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <p className="mb-3 text-2xl font-extrabold tracking-wide text-gray-900 dark:text-white">
-          {`سلام  "دوست"} عزیز 👋`}
+          {`سلام  "دوست" عزیز 👋`}
         </p>
         <p className="mb-4 text-md font-medium text-gray-700 leading-relaxed dark:text-gray-400 ">
           از دیدن شما در پنل کاربری‌مون خوشحالیم! 🎉{" "}
-          <span className="font-semibold text-indigo-600">
+          <span className="font-semibold text-color1">
             با تکمیل پروفایل‌تون
           </span>
           ، تجربه‌ای کامل‌تر و حرفه‌ای‌تر از خدمات ما خواهید داشت. با ما همراه
