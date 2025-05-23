0a004388cde55a3bf457be37e8f3953a23cb1a76
AbbasRostami <abbas.cracker@gmail.com>
2025-05-24 00:46:32 +0330

    Implement FavoritesPage component with a responsive table for saved bookings, featuring sorting, filtering, and action dropdowns. Enhance user experience with dynamic status displays and pagination controls.

--- src/app/(dashboard)/favorites/page.tsx
+++ src/app/(dashboard)/favorites/page.tsx
@@ -1,7 +1,421 @@
+"use client";
+
+import {
+  Badge,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Pagination,
+} from "@heroui/react";
+import {
+  ColumnDef,
+  useReactTable,
+  getCoreRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+  flexRender,
+  getPaginationRowModel,
+  OnChangeFn,
+  SortingState,
+} from "@tanstack/react-table";
+import { useMemo, useState } from "react";
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
+import { TiDeleteOutline } from "react-icons/ti";
+import { PiWarningCircleBold } from "react-icons/pi";
+import { GiWallet } from "react-icons/gi";
+import { HiDotsHorizontal } from "react-icons/hi";
+import { FaPlusCircle } from "react-icons/fa";
+import image from "./../../../assets/Avatar1.png";
+import image2 from "./../../../assets/Avatar2.png";
+import image3 from "./../../../assets/Avatar3.png";
+import Image from "next/image";
+interface BookingData {
+  id: number;
+  title: string;
+  date: string;
+  price: number;
+  guests: number;
+  status: "تایید شده" | "در انتظار" | "لغو شده";
+  payment_status: "تایید شده" | "لغو شده";
+  image: string;
+}
+
 export default function FavoritesPage() {
+  const [sorting, setSorting] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
+  const columns = useMemo<ColumnDef<BookingData>[]>(
+    () => [
+      {
+        accessorKey: "id",
+        header: "ردیف",
+        cell: (info) => info.row.original.id,
+        sortingFn: (rowA, rowB) => rowA.original.id - rowB.original.id,
+      },
+      {
+        accessorKey: "image",
+        header: "تصویر",
+        cell: (info) => (
+          <Image
+            src={info.getValue() as string}
+            alt="image"
+            width={42}
+            height={42}
+            className="rounded-full"
+          />
+        ),
+      },
+      {
+        accessorKey: "title",
+        header: "نام اقامتگاه",
+        cell: (info) => info.getValue(),
+        enableSorting: true,
+      },
+      {
+        accessorKey: "date",
+        header: "تاریخ رزرو",
+        enableSorting: false,
+      },
+      {
+        accessorKey: "price",
+        header: "قیمت کل",
+        cell: (info) => `${(+info.getValue()).toLocaleString()} تومان`,
+        enableSorting: true,
+        sortingFn: (rowA, rowB, columnId) =>
+          (rowA.getValue(columnId) as number) -
+          (rowB.getValue(columnId) as number),
+      },
+      {
+        accessorKey: "guests",
+        header: "تعداد مسافر",
+        enableSorting: true,
+        sortingFn: (rowA, rowB, columnId) =>
+          (rowA.getValue(columnId) as number) -
+          (rowB.getValue(columnId) as number),
+      },
+      {
+        accessorKey: "status",
+        header: "وضعیت رزرو",
+        cell: (info) => {
+          const value = info.getValue();
+          return (
+            <p
+              className={`p-1 text-medium font-normal rounded-2xl ${
+                value === "تایید شده"
+                  ? "badge-success"
+                  : value === "در انتظار"
+                  ? "badge-warning"
+                  : "badge-danger"
+              }`}
+            >
+              {value as string}
+            </p>
+          );
+        },
+        enableSorting: true,
+      },
+      {
+        accessorKey: "payment_status",
+        header: "وضعیت پرداخت",
+        cell: (info) => {
+          const value = info.getValue();
+
+          return (
+            <p
+              className={`p-1 px-2 text-medium font-normal rounded-2xl ${
+                value === "تایید شده" ? "badge-success" : "badge-danger"
+              }`}
+            >
+              {value as string}
+            </p>
+          );
+        },
+        enableSorting: true,
+      },
+      {
+        accessorKey: "actions",
+        header: "عملیات",
+        cell: (info) => {
+          return (
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="light">
+                  <HiDotsHorizontal size={20} />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem color="success" key="payment">
+                  <div className="flex items-center gap-2">
+                    <GiWallet size={20} />
+                    پرداخت
+                  </div>
+                </DropdownItem>
+                <DropdownItem color="warning" key="details">
+                  <div className="flex items-center gap-2">
+                    <PiWarningCircleBold size={20} />
+                    جزئیات
+                  </div>
+                </DropdownItem>
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                >
+                  <div className="flex items-center gap-2">
+                    <TiDeleteOutline size={20} />
+                    حذف
+                  </div>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          );
+        },
+        enableSorting: false,
+      },
+    ],
+    []
+  );
+
+  const data: BookingData[] = [
+    {
+      id: 1,
+      title: "هتل سراوان",
+      date: "1403/02/01/ 10:00",
+      price: 150000000,
+      guests: 88,
+      status: "تایید شده",
+      payment_status: "لغو شده",
+      image: image.src,
+    },
+    {
+      id: 2,
+      title: "شیراز پارک",
+      date: "1403/02/01/ 10:00",
+      price: 150000000,
+      guests: 70,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image2.src,
+    },
+    {
+      id: 3,
+      title: "تراول پارک",
+      date: "1403/02/01/ 10:00",
+      price: 160000000,
+      guests: 53,
+      status: "در انتظار",
+      payment_status: "لغو شده",
+      image: image3.src,
+    },
+    {
+      id: 4,
+      title: "میدان جمهریه",
+      date: "1403/02/01/ 10:00",
+      price: 180000000,
+      guests: 10,
+      status: "تایید شده",
+      payment_status: "لغو شده",
+      image: image2.src,
+    },
+    {
+      id: 5,
+      title: "ماهی پارک",
+      date: "1403/02/01/ 10:00",
+      price: 170000000,
+      guests: 7,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image3.src,
+    },
+    {
+      id: 6,
+      title: "کوه سراوان",
+      date: "1403/02/01/ 10:00",
+      price: 170000000,
+      guests: 38,
+      status: "در انتظار",
+      payment_status: "لغو شده",
+      image: image2.src,
+    },
+    {
+      id: 7,
+      title: "ساحل سراوان",
+      date: "1403/02/01/ 10:00",
+      price: 100000,
+      guests: 85,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image.src,
+    },
+    {
+      id: 8,
+      title: "ماهی پارک",
+      date: "1403/02/01/ 10:00",
+      price: 160000000,
+      guests: 741,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image2.src,
+    },
+    {
+      id: 9,
+      title: "ماهی پارک",
+      date: "1403/02/01/ 10:00",
+      price: 190000000,
+      guests: 52,
+      status: "تایید شده",
+      payment_status: "لغو شده",
+      image: image3.src,
+    },
+    {
+      id: 10,
+      title: "نسرین پارک",
+      date: "1403/02/01/ 10:00",
+      price: 170000000,
+      guests: 976,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image2.src,
+    },
+    {
+      id: 11,
+      title: "ماهی پارک",
+      date: "1403/02/01/ 10:00",
+      price: 170000000,
+      guests: 52,
+      status: "در انتظار",
+      payment_status: "لغو شده",
+      image: image.src,
+    },
+    {
+      id: 12,
+      title: "ساحل سراوان",
+      date: "1403/02/01/ 10:00",
+      price: 170000000,
+      guests: 5,
+      status: "در انتظار",
+      payment_status: "تایید شده",
+      image: image3.src,
+    },
+    {
+      id: 13,
+      title: "ماهی بهشهر",
+      date: "1403/02/01/ 10:00",
+      price: 186600000,
+      guests: 48,
+      status: "تایید شده",
+      payment_status: "لغو شده",
+      image: image2.src,
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: { sorting, globalFilter, pagination },
+    onSortingChange: setSorting as OnChangeFn<SortingState>,
+    onGlobalFilterChange: setGlobalFilter,
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    autoResetPageIndex: false,
+  });
+
   return (
-    <main className="p-6">
-      <h1 className="text-xl font-bold mb-4">لیست علاقه مندی‌ها</h1>
-    </main>
+    <div className="space-y-4">
+      <div className="flex items-center justify-between gap-2 pb-6 border-b-2 border-dashed border-amber-500">
+        <div className="flex items-center gap-2">
+          <FaPlusCircle
+            className="text-amber-900 dark:text-amber-200"
+            size={30}
+          />
+          <span className="text-amber-500 text-xl font-bold  dark:text-amber-200 pb-3 border-b-4 border-amber-500 relative group transition-all duration-300 ease-in-out">
+            لیست رزرو های ذخیره شده
+          </span>
+        </div>
+        <input
+          type="text"
+          placeholder="نام اقامتگاه مورد نظر را جستجو کنید..."
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          className="w-1/3 p-2 rounded-md border-2 border-amber-500"
+        />
+      </div>
+
+      <div className="overflow-x-auto  rounded-xl">
+        <table className="min-w-full  table-auto text-sm">
+          <thead className="bg-amber-200/70 dark:bg-gray-500 text-center">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    onClick={header.column.getToggleSortingHandler()}
+                    className="p-4  font-bold cursor-pointer text-center select-none"
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                    {header.column.getIsSorted() === "asc" && (
+                      <BsArrowUp className="inline w-4 h-4 ml-1" />
+                    )}
+                    {header.column.getIsSorted() === "desc" && (
+                      <BsArrowDown className="inline w-4 h-4 ml-1" />
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+            {table.getRowModel().rows.map((row, index) => (
+              <tr
+                key={row.id}
+                className={`
+            ${
+              index % 2 === 0
+                ? "bg-blue-50 dark:bg-gray-800/80"
+                : "bg-white dark:bg-gray-700/80"
+            }
+            hover:bg-amber-100/70 dark:hover:bg-gray-600
+            transition-colors duration-200
+            text-center
+          `}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    className="p-3 text-gray-700 dark:text-gray-300 whitespace-nowrap"
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-end">
+        <Pagination
+          dir="ltr"
+          color="warning"
+          isCompact
+          showControls
+          total={table.getPageCount()}
+          page={table.getState().pagination.pageIndex + 1}
+          onChange={(page) => table.setPageIndex(page - 1)}
+        />
+      </div>
+    </div>
   );
 }
