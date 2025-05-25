71eb72fd7b0018e074f13969ae98f5e42744c5c0
AbbasRostami <abbas.cracker@gmail.com>
2025-05-26 02:02:25 +0330

    Refactor UserDropdown Component
    - Removed unused modal and notification settings functionality to streamline the UserDropdown component.
    - Updated user description from "خریدار" to "فروشنده" and changed wallet charging text to "موجودی قابل برداشت" for improved clarity.
    - Enhanced overall component structure for better performance and user experience.

--- src/app/(seller)/components1/header/UserDropdown.tsx
+++ src/app/(seller)/components1/header/UserDropdown.tsx
@@ -3,23 +3,14 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalHeader,
-  Switch,
-  useDisclosure,
   User,
 } from "@heroui/react";
-import { FaBell, FaPlusCircle, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { FaPlusCircle, FaSignOutAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { GiRingingBell } from "react-icons/gi";
-import { IoMdClose } from "react-icons/io";
+
 export default function UserDropdown() {
-  const router = useRouter();
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <>
       <Dropdown placement="bottom-start">
@@ -31,7 +22,7 @@ export default function UserDropdown() {
               src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
             }}
             className="transition-transform"
-            description="خریدار"
+            description="فروشنده"
             name="عباس رستمی"
           />
         </DropdownTrigger>
@@ -53,32 +44,13 @@ export default function UserDropdown() {
             />
           </DropdownItem>
 
-          <DropdownItem key="settings" textValue="شارژ کردن کیف پول">
+          <DropdownItem color="primary" key="settings" textValue="موجودی قابل برداشت">
             <div className="flex items-center gap-2">
               <FaPlusCircle />
-              شارژ کردن کیف پول
+              موجودی قابل برداشت
             </div>
           </DropdownItem>
-          <DropdownItem
-            onPress={onOpen}
-            key="team_settings"
-            textValue="تنظیمات نوتیفیکیشن ها"
-          >
-            <div className="flex items-center gap-2">
-              <FaBell />
-              تنظیمات نوتیفیکیشن ها
-            </div>
-          </DropdownItem>
-          <DropdownItem
-            key="system"
-            onPress={() => router.push("/profile")}
-            textValue="ویرایش اطلاعات کاربری"
-          >
-            <div className="flex items-center gap-2">
-              <FaUser />
-              ویرایش اطلاعات کاربری
-            </div>
-          </DropdownItem>
+
           <DropdownItem
             textValue="خروج از حساب کاربری"
             key="logout"
@@ -119,47 +91,6 @@ export default function UserDropdown() {
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        size="2xl"
-        hideCloseButton
-      >
-        <ModalContent>
-          <>
-            <ModalHeader className="flex justify-between items-center gap-2  text-2xl  font-normal">
-              <div className="flex items-center gap-2">
-                <GiRingingBell />
-                تنظیمات نوتیفیکیشن
-              </div>
-              <button
-                className="flex items-center gap-2 border border-red-400 text-red-500 rounded-full px-6 py-2 text-lg font-bold hover:bg-red-50 transition"
-                onClick={onOpenChange}
-              >
-                بستن <IoMdClose size={24} />
-              </button>
-            </ModalHeader>
-            <ModalBody className="flex flex-col gap-10 text-right border-dotted border-t-2 py-4 border-color1">
-              <div className="flex items-center justify-between flex-row">
-                <span className="text-xl font-normal">نوتیفیکیشن رزرو</span>
-                <Switch defaultSelected color="warning" />
-              </div>
-              <div className="flex items-center justify-between flex-row">
-                <span className="text-xl font-normal">نوتیفیکیشن پرداخت</span>
-                <Switch color="warning" />
-              </div>
-              <div className="flex items-center justify-between flex-row">
-                <span className="text-xl font-normal">نوتیفیکیشن تخفیف</span>
-                <Switch defaultSelected color="warning" />
-              </div>
-              <div className="flex items-center justify-between flex-row">
-                <span className="text-xl font-normal">نوتیفیکیشن سیستمی</span>
-                <Switch defaultSelected color="warning" />
-              </div>
-            </ModalBody>
-          </>
-        </ModalContent>
-      </Modal>
     </>
   );
 }
