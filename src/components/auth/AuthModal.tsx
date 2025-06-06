f335370f436cd0c0380f31d140ed6fbcade48f72
AbbasRostami <abbas.cracker@gmail.com>
2025-06-07 00:58:07 +0330

    Remove unnecessary whitespace in AuthModal component for cleaner code.

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -146,7 +146,6 @@ export default function AuthModal({
                     />
                   </>
                 )}
-                
               </div>
 
               {/* Desktop Register/Login Tabs */}
